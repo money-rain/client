b25abdafcc25f05addfab12e98a8f4a797980d2c
abedlubis <lubisabednego@gmail.com>
2019-01-24 21:46:18 +0700

    sound on

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,38 @@
 <template>
   <v-app>
-    <v-content>
-      <router-view></router-view>
-    </v-content>
+    <router-view/>
+    <audio v-if="soundOn"
+        autoplay
+        :src="backsound">
+            Your browser does not support the
+            <code>audio</code> element.
+    </audio>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
+import HelloWorld from './components/HelloWorld'
+import { mapState } from 'vuex'
 
 export default {
-  name: "App",
-  components: {
+  name: 'App',
+  mounted() {
+    
   },
-  data() {
+  computed: mapState([
+    // map this.count to store.state.count
+    'backsound',
+    'soundOn'
+  ]),
+  data () {
     return {
-      //
-    };
+
+    }
+  },
+  methods : {
+    cing () {
+        
+    }
   }
 };
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -17,6 +17,7 @@
                           single-line
                           box
             ></v-text-field>
+            <v-btn color="primary" @click.prevent="cing"> Click </v-btn>
           </v-form>
         </v-flex>
       </v-layout>
@@ -26,26 +27,42 @@
 </template>
 
 <script>
+import { mapState } from 'vuex'
+import { mapActions } from 'vuex'
 
-  export default {
+export default {
     name : 'Home',
+    computed: mapState([
+      'soundOn'
+    ]),
+    mounted() {
+      
+    },
     data: () => ({
-        name : ''
+        name : '',
+        coin: 'https://freesound.org/people/cabled_mess/sounds/350873/download/350873__cabled-mess__coin-c-02.wav'
     }),
     methods : {
+      ...mapActions([
+        'soundOff'
+      ]),
       addUsername () {
         this.$db.collection("users").add({
             name: this.name,
             point: 0
         })
         .then((docRef) => {
-            this.$router.push('/rooms');
+          this.$router.push('/rooms');
         })
         .catch((error) => {
             console.error("Error adding document: ", error);
         });
-
+      },
+      cing() {
+        this.soundOff()
       }
-    }
+      
+    },
+    
   }
 </script>
